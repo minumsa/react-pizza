e260a6bf4ce23bee1ce934abd1fe0dd0c6a14d09
유일 <83910706+minumsa@users.noreply.github.com>
2021-06-05 12:50:02 +0900

    Final

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -5,6 +5,7 @@ import MapAPI from '../components/MapAPI';
 import '../styles/Location.css';
 import PizzaLeft from '../assets/pizzaLeft.jpg'
 import { Link } from 'react-router-dom';
+import map from '../assets/map.png'
 
 function Location() {
   return (
@@ -13,7 +14,7 @@ function Location() {
       <div className="rightSide">
         <div className="kakaomap">
           <h1>OUR LOCATION</h1>
-          <MapAPI />
+          <img src={ map } className="mapImage"/>
           <p className="kakaoAddress">Seoul, Republic of Korea</p>
           <Link to="/menu">
           <button>ORDER NOW</button>
