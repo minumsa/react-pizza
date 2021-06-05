6eed5e99b9342f4bf1ef0208b35269e90ab5c620
유일 <83910706+minumsa@users.noreply.github.com>
2021-06-05 11:10:08 +0900

    Home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import '../styles/Footer.css';
+import { Twitter } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
+      </div>
+      <p> &copy; 2021 yonghunpizza.com </p>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import mainImage from '../assets/pizza.jpeg';
+import '../styles/Home.css';
+import { Link } from 'react-router-dom';
 
 function home() {
   return (
-    <div>
-      Home
+      <div className="home" style={{ backgroundImage: `url(${mainImage})` }}>
+        <div className="headerContainer">
+          <h1>Yonghun's Pizza</h1>
+          <p>PIZZA TO FIT ANY TASTE</p>
+          <Link to="/menu">
+          <button>ORDER NOW</button>
+          </Link>
+      </div>
     </div>
   )
 }
