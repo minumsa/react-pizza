6d658d02008e509e692ece6e38ad508c84f592ea
유일 <83910706+minumsa@users.noreply.github.com>
2021-06-05 10:29:53 +0900

    components, pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Location from './pages/Location';
 
 function App() {
   return (
     <div className="App">
-      용헌이
+      <Router>
+        <Navbar />
+          <Switch>
+            <Route exact component={Home} path="/" />
+            <Route exact component={Menu} path="/menu" />
+            <Route exact component={About} path="/about" />
+            <Route exact component={Location} path="/location" />
+        </Switch>
+        <Footer />
+      </Router>
+
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Logo from '../assets/pizzaLogo.png';
+import { Link } from 'react-router-dom';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
+
+const [openLinks, setOpenLinks] = useState(false);
+const toggleNavbar = ()=>{
+  setOpenLinks(!openLinks);
+};
+
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <div className="leftSide">
+        <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to ="/">Home</Link>
+          <Link to ="/menu">Menu</Link>
+          <Link to ="/about">About</Link>
+          <Link to ="/location">Location</Link>
+        </div>
+      </div>
+        <div className="rightSide">
+          <Link to ="/">Home</Link>
+          <Link to ="/menu">Menu</Link>
+          <Link to ="/about">About</Link>
+          <Link to ="/location">Location</Link>
+          <button onClick={ toggleNavbar }>
+            <ReorderIcon />
+          </button>
+      </div>
     </div>
   )
 }
