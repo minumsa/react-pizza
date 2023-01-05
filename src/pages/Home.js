13625463c5c69aed967f396c47029d39a5f6c875
minumsa <icycyi@naver.com>
2023-01-05 18:12:30 +0900

    About.js CSS 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import mainImage from '../assets/pizza.jpeg';
-import '../styles/Home.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import mainImage from "../assets/pizza.jpeg";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
 
 function home() {
   return (
-      <div className="home" style={{ backgroundImage: `url(${mainImage})` }}>
-        <div className="headerContainer">
-          <h1>Yonghun's Pizza</h1>
-          <p>PIZZA TO FIT ANY TASTE</p>
-          <Link to="/menu">
+    <div className="home" style={{ backgroundImage: `url(${mainImage})` }}>
+      <div className="headerContainer">
+        <h1>Carver's Pizza</h1>
+        <p>PIZZA TO FIT ANY TASTE</p>
+        <Link to="/menu">
           <button>ORDER NOW</button>
-          </Link>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default home
+export default home;
